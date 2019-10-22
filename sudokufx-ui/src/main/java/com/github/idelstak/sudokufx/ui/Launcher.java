76f225d37f831443c3c8bac18f42962258612b55
Hiram K. <hiram.kamau@outlook.com>
2019-10-22 18:10:26 +0300

    Make parent project runnable in ide

--- sudokufx-ui/src/main/java/com/github/idelstak/sudokufx/ui/Launcher.java
+++ sudokufx-ui/src/main/java/com/github/idelstak/sudokufx/ui/Launcher.java
@@ -3,7 +3,22 @@
  */
 package com.github.idelstak.sudokufx.ui;
 
+import javafx.application.Application;
+
 /**
+ The Java 13 runtime will check if the main class extends {@link Application},
+ and if that is the case, it strongly requires the JavaFX platform to be
+ available as a module, and not as a jar for example.
+ <p>
+ While going modular is definitely the way to go, I still think it's unfortunate
+ that JavaFX applications are treated a bit different unlike other applications
+ (where you can still use the classpath).
+ <p>
+ There are some easy workarounds though. For example, you can have a main class
+ (i.e. {@link Launcher}) that is not extending JavaFX.application.Application,
+ and that main class can then call the main(String[]) method on your real main
+ class --i.e., {@link Main} (that way, the Java launcher doesn't require the
+ JavaFX libraries to be available as named modules).
 
  @author Hiram K <hiram.kamau@outlook.com>
  */
